ef75978676ee475e7d92e34bdd69759ec979908c
Ousssama El-Amrani <oussama.maroc8@gmail.com>
2025-12-08 18:43:29 +0100

    feat(command): add controller to view the account events

--- src/main/java/me/elamranioussama/eventsourcingcqrsaxon/command/controllers/AccountCommandController.java
+++ src/main/java/me/elamranioussama/eventsourcingcqrsaxon/command/controllers/AccountCommandController.java
@@ -3,18 +3,22 @@ package me.elamranioussama.eventsourcingcqrsaxon.command.controllers;
 import me.elamranioussama.eventsourcingcqrsaxon.command.commands.AddAccountCommand;
 import me.elamranioussama.eventsourcingcqrsaxon.command.dtos.AddNewRequestDTO;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.eventsourcing.eventstore.EventStore;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/commands/accounts")
 public class AccountCommandController {
     private final CommandGateway commandGateway;
+    private final EventStore eventStore;
 
-    public AccountCommandController(CommandGateway commandGateway) {
+    public AccountCommandController(CommandGateway commandGateway, EventStore eventStore) {
         this.commandGateway = commandGateway;
+        this.eventStore = eventStore;
     }
 
     @PostMapping("/add")
@@ -33,5 +37,10 @@ public class AccountCommandController {
     @ExceptionHandler(Exception.class)
     public String exceptionHandler(Exception exception) {
         return exception.getMessage();
+    }
+
+    @GetMapping("/events/{accountid}")
+    public Stream eventStore(@PathVariable String accountid) {
+        return eventStore.readEvents(accountid).asStream();
     }
 }
